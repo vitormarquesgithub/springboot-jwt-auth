54f59760127928309cae71a3fd57242c376c6221
Vitor Marques <vitormarquesdev@gmail.com>
2026-07-12 14:54:23 +0000

    chore: standardize script output messages and emojis

--- scripts/docker-rebuild.sh
+++ scripts/docker-rebuild.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 source ./scripts/colors.sh
 
-echo -e "${RED}🗘 Rebuilding images (no cache)...${NC}"
+echo -e "${RED}⟳ Rebuilding images (no cache)...${NC}"
 docker-compose -f ./docker/docker-compose.yml build --no-cache
 ./scripts/docker-up.sh
 echo -e "${GREEN}✔︎ Rebuild complete and containers are running!${NC}"
